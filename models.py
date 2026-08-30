b74a45f8d90e108ed8f62ac2b47d71a06d57c916
Sajeela Noor <i.sajeela.noor@gmail.com>
2026-08-30 19:51:15 +0500

    Remove circular import in models.py

--- models.py
+++ models.py
@@ -2,7 +2,6 @@ from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 import models
 import schemas
-import auth
 from auth import get_db, require_student
 from datetime import date, datetime
 import logging
