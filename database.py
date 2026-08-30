61b3a17b568ce75de342d46bbf44c49bd19c351d
Sajeela Noor <i.sajeela.noor@gmail.com>
2026-08-30 20:17:08 +0500

    Read DATABASE_URL from environment for Postgres support

--- database.py
+++ database.py
@@ -1,11 +1,13 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "sqlite:///./campus_routier.db"
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./campus_routier.db")
 
-engine = create_engine(
-    DATABASE_URL, connect_args={"check_same_thread": False}
-)
+# check_same_thread only applies to SQLite - Postgres doesn't need/want it
+connect_args = {"check_same_thread": False} if DATABASE_URL.startswith("sqlite") else {}
+
+engine = create_engine(DATABASE_URL, connect_args=connect_args)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
